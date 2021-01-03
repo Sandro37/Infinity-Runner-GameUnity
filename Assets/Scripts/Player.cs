dd38cc55136d5a4394bd7b54640469542b7740de
Sandro37 <alessandro37@outlook.com.br>
2021-01-02 22:13:34 -0300

    Plataformas infinitas. Animação do player. Movimento do player. Pulo do Player
    Foi feita as animações, cenário infinito,  movimento sempre para direita e efeito de pulo. Neste commit, ele pula só uma vez.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float velocity;
+    public float jumpForce;
+    private bool isJump;
+
+    private  Rigidbody2D rig;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jump(); 
+    }
+    private void FixedUpdate()
+    {
+        move();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
+    }
+
+    private void move()
+    {
+        rig.velocity = new Vector2(velocity, rig.velocity.y);
+    }
+
+    private void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+        {
+            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetBool("isJump", true);
+            isJump = true;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnPlatform.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlatform : MonoBehaviour
+{
+    public List<GameObject> platforms = new List<GameObject>(); // lista das plataformas geradas na cena
+    private List<Transform> currentsPlatforms = new List<Transform>(); // lista dos prefabs das plataformas
+    private Transform player;
+    private Transform currentPlatformPoint;
+    private int platformIndex;
+
+    public float distance_platform;
+    public float defaultValueDistance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        for(int i = 0; i < platforms.Count; i++)
+        {
+            Transform trans = Instantiate(platforms[i],new Vector2(i * defaultValueDistance, -5), transform.rotation).transform;
+            currentsPlatforms.Add(trans);
+            distance_platform += 30f;
+        }
+
+        currentPlatformPoint = currentsPlatforms[platformIndex].GetComponent<Platform>().final_point;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    private void move()
+    {
+        float distance = player.position.x - currentPlatformPoint.position.x;
+
+        if (distance >= 1)
+        {
+            recycle(currentsPlatforms[platformIndex].gameObject);
+            platformIndex++;
+
+            if(platformIndex > platforms.Count - 1)
+            {
+                platformIndex = 0;
+            }
+            currentPlatformPoint = currentsPlatforms[platformIndex].GetComponent<Platform>().final_point;
+        }
+    }
+    private void recycle(GameObject platform)
+    {
+        platform.transform.position = new Vector2(distance_platform, -5);
+        distance_platform += 30f;
+    }
+}
