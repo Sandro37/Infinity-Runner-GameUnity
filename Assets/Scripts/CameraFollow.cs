cf241260d05f3919156cd49645b6e1d9200ead86
Sandro37 <alessandro37@outlook.com.br>
2021-01-02 23:27:08 -0300

    Movimentação da câmera - Script
    Criado algoritmo para câmera seguir o personagem no cenário, indo na posição X Positiva.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform player;
+    public float speedCamera;
+    public float offSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        Vector3 newCampPos = new Vector3(player.position.x + offSet, 0f, transform.position.z);
+        transform.position = Vector3.Slerp(transform.position, newCampPos, speedCamera * Time.deltaTime);
+    }
+}
